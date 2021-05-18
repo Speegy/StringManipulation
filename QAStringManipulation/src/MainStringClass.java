5c83b88992098d8ce913e4ca4be94fd325e2f4d6
Speegy <mcjakedude@gmail.com>
2021-05-18 12:50:09 -0600

    String problems

--- QAStringManipulation/src/MainStringClass.java
+++ QAStringManipulation/src/MainStringClass.java
@@ -3,18 +3,73 @@ public class MainStringClass {
 
 	public static String str1 = "yesterday it was raining";
 	public static String str2 = "today it is sunny";
+	public static String str3 = "today it was raining";
+	public static String str4 = "it was raining";
 	
 	public static void main(String[] args) {
-		System.out.println(concatCapitalize());
+//		System.out.println(concatCapitalize().substring(0, 6) + concatCapitalize().substring(29));
+//		System.out.println(countWords(concatCapitalize()));
+//		System.out.println(printWordsOnNewLineReverse(concatCapitalize()));
+		System.out.println(findSubString(str3, str4));
 	}
 	
 	public static String concatCapitalize() {
 		return str2.toUpperCase()+", ".concat(str1).toUpperCase();
 	}
 	
+	public static int countWords(String str) {
+		int count = 0;
+		if(str.length() > 0) {
+			count += 1;
+			for(int i = 0; i < str.length(); i++) {
+				if(str.substring(i, i+1).equals(" ")) count+=1;
+			}			
+		}
+		return count;
+	}
 	
+	public static String printWordsOnNewLine(String str) {
+		if(str.length() > 0) {
+			for(int i = 0; i < str.length(); i++) {
+				if(str.substring(i, i+1).equals(" ")) {
+					System.out.println();
+					i+=1;
+				}
+				System.out.print(str.substring(i, i+1));
+			}			
+		}
+		return "";
+	}
 	
-
+	public static String printWordsOnNewLineReverse(String str) {
+		int tempCount = str.length();
+		if(str.length() > 0) {
+			for(int i = str.length()-1; i >= 0; i--) {
+				if(str.substring(i, i+1).equals(" ") || i == 0) {
+					if(i == 0) {
+						System.out.println(str.substring(i, tempCount));
+					}else {
+						System.out.println(str.substring(i+1, tempCount));						
+					}
+					tempCount = i;
+				}
+			}			
+		}
+		return "";
+	}
+	
+	public static boolean findSubString(String str1, String str2) {
+		boolean bool = false;
+		for(int i = 0; i < str1.length(); i++) {
+//			if(str1.substring(i, i+1).equals(str2.substring(0, 1))) {
+			if(str1.substring(i, i+str2.length()).equals(str2.substring(0, str2.length()))) {
+				bool = true;
+			}
+//			}
+		}
+		return bool;
+	}
+	
 }
 
 //
